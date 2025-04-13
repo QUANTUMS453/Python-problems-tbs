cc20b916beca91ce5dcd4efff52aa2091ee85ab4
QUANTUMS453 <hossein543251@gmail.com>
2025-04-13 23:05:46 +0330

    New problem

---
+++ Easy/Longest_word.py
@@ -0,0 +1,11 @@
+import string
+
+def longest_word(str1: str) -> str:
+    cleared = str1.split()
+    cleaned_words = [word.strip(string.punctuation) for word in cleared]
+    longest = max(cleaned_words, key=len)
+    print(f"The longest word is: {longest}")
+    return longest
+
+# Example usage
+longest_word("mammad is a good friend foor him and he should keep the friendship up......")
